37b425f4888cd8d79c0f93095a42bd35d79a29bd
cmadrid48 <cmadrid48@gmail.com>
2023-01-22 18:59:14 -0800

    started js using jq,and dayJs. cool stuff

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -2,26 +2,27 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-  
-  
-    
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+  $('.saveBtn').click(function() {
+    var blockId = $(this).parent('.time-block').attr('id');
+    var content = $(this).parent('.time-block').find('textarea').val();
+    localStorage.setItem(blockId, content);
+    console.log(content, blockId)
   });
+
+  $('.time-block').each(function() {
+    var hour = $(this).attr('id').split('-')[1];
+
+      if (hour < dayjs().hour()) {
+        $(this).addClass('past');
+      } else if (hour == dayjs().hour()) {
+        $(this).addClass('present');
+      } else {
+        $(this).addClass('future');
+      }
+
+      $(this).find('textarea').text(localStorage.getItem($(this).attr('id')));
+  });
+
+  
+});
   
