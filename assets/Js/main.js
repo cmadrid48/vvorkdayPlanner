2cd9997b47d4778f178bf1a4d937e1891462a61b
cmadrid48 <cmadrid48@gmail.com>
2023-01-23 17:53:45 -0800

    added comments

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+// save button function
 $(function () {
   $('.saveBtn').click(function() {
     var blockId = $(this).parent('.time-block').attr('id');
@@ -9,6 +12,7 @@ $(function () {
     console.log(content, blockId)
   });
 
+//Live moving block function
   $('.time-block').each(function() {
     var hour = $(this).attr('id').split('-')[1];
 
@@ -23,6 +27,7 @@ $(function () {
       $(this).find('textarea').text(localStorage.getItem($(this).attr('id')));
   });
 
+//displaying time using daysjs
   $('#currentDay').text(dayjs().format('dddd, MMMM D, h:mm:s A'));
 });
   
